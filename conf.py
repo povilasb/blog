07cd5f51b5b75460f17dfcbb0ecb3e78ac70fbf1
Povilas Balciunas <balciunas90@gmail.com>
2017-06-18 22:23:04 +0300

    Set infinite number of posts on index page

--- conf.py
+++ conf.py
@@ -1012,7 +1012,8 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+import sys
+INDEX_DISPLAY_POST_COUNT = sys.maxsize
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
